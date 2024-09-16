9ee9f3ab8d20a5f7f7b98b6810b2c051afa2cabe
B. Emmanuel Pericon <brianpericon.e92@gmail.com>
2024-09-16 17:14:12 -0300

    feat: added attack animation

--- src/entities/player/Player.gd
+++ src/entities/player/Player.gd
@@ -1,25 +1,33 @@
 extends CharacterBody2D
 
-@export var ACCELERATION:float = 300.0
-@export var H_SPEED_LIMIT:float = 600.0
+@export var ACCELERATION:float = 50.0
+@export var SPEED_LIMIT:float = 600.0
 @export var FRICTION_WEIGHT:float = 1.1
 @export var JUMP_VELOCITY:float = -400.0
 
 @onready var _body_pivot: Node2D = $BodyPivot
 @onready var _body_animations = $BodyAnimations
 
+var is_moving:bool = false
+var is_attacking:bool = false
+var is_jumping:bool = false
+
 func _ready() -> void:
-	_body_animations.play("idle")
+	pass
 
 
 func _physics_process(delta: float) -> void:
+	
+	is_attacking = _body_animations.current_animation == "attack_01"
 	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
 	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITYInput.is_action_pressed("ui_right")
+	if Input.is_action_just_pressed("jump"):# and is_on_floor():
+		print("jumping")
+		is_jumping = true
+		velocity.y = JUMP_VELOCITY
 	
 	# Player movement
 	var h_movement_direction:int = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
@@ -27,35 +35,45 @@ func _physics_process(delta: float) -> void:
 	
 	# Horirizontal control
 	if h_movement_direction != 0:
-		velocity.x = clamp(velocity.x + (h_movement_direction * ACCELERATION), -H_SPEED_LIMIT, H_SPEED_LIMIT)
+		velocity.x = clamp(velocity.x + (h_movement_direction * ACCELERATION), -SPEED_LIMIT, SPEED_LIMIT)
 		_body_pivot.scale.x = 1 - 2 * float(h_movement_direction < 0)
 	else:
 		velocity.x = lerp(velocity.x, 0.0, FRICTION_WEIGHT) if abs(velocity.x) > 1 else 0
 	# Vertical control
 	if v_movement_direction != 0:
-		velocity.y = clamp(velocity.y + (v_movement_direction * ACCELERATION), -H_SPEED_LIMIT, H_SPEED_LIMIT)
+		velocity.y = clamp(velocity.y + (v_movement_direction * ACCELERATION), -SPEED_LIMIT, SPEED_LIMIT)
 	else:
+		#print("v_movement_direction != 0 else")
 		velocity.y = lerp(velocity.y, 0.0, FRICTION_WEIGHT) if abs(velocity.y) > 1 else 0
 	
-	if Input.is_action_just_pressed("attack_01"):
-		_play_animation("attack_01")
 	
-	if not _body_animations.is_playing():
-		if velocity != Vector2.ZERO:
-			_play_animation("run")
-			move_and_slide()
-		else:
-			_play_animation("idle")
-			
-	#if h_movement_direction != 0 || v_movement_direction != 0:
-		#_play_animation("run")
-		#move_and_slide()
-	#elif Input.is_action_just_pressed("attack_01"):
-		#_play_animation("attack_01")
-	#else:
-		#_play_animation("idle")
+	
+	if h_movement_direction != 0 or v_movement_direction != 0:
+		move()
+	elif Input.is_action_just_pressed("attack_01"):
+		attack()
+	elif not is_moving and not is_attacking:
+		_play_animation("idle")
+		
 
 	
 func _play_animation(animation: String) -> void:
 	if _body_animations.has_animation(animation):
 		_body_animations.play(animation)
+
+
+func move() -> void:
+	is_moving = true
+	_play_animation("run")
+	move_and_slide()
+	is_moving = false
+
+
+func attack() -> void:
+	is_attacking = true
+	_play_animation("attack_01")
+	is_attacking = false
+
+
+func _on_jump_buffer_timer_timeout() -> void:
+	pass # Replace with function body.
