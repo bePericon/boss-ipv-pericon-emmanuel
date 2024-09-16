c4785ba0c003efabd71a6d61d6da980f146177f4
B. Emmanuel Pericon <brianpericon.e92@gmail.com>
2024-09-16 19:35:03 -0300

    feat: some progress about jump

--- src/entities/player/Player.gd
+++ src/entities/player/Player.gd
@@ -3,13 +3,15 @@ extends CharacterBody2D
 @export var ACCELERATION:float = 50.0
 @export var SPEED_LIMIT:float = 600.0
 @export var FRICTION_WEIGHT:float = 1.1
-@export var JUMP_VELOCITY:float = -400.0
+@export var JUMP_VELOCITY:float = 400.0
 
 @onready var _body_pivot: Node2D = $BodyPivot
 @onready var _body_animations = $BodyAnimations
 
 var is_moving:bool = false
 var is_attacking:bool = false
+
+
 var is_jumping:bool = false
 
 func _ready() -> void:
@@ -17,17 +19,9 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	
-	is_attacking = _body_animations.current_animation == "attack_01"
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump"):# and is_on_floor():
-		print("jumping")
-		is_jumping = true
-		velocity.y = JUMP_VELOCITY
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
 	
 	# Player movement
 	var h_movement_direction:int = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
@@ -39,22 +33,34 @@ func _physics_process(delta: float) -> void:
 		_body_pivot.scale.x = 1 - 2 * float(h_movement_direction < 0)
 	else:
 		velocity.x = lerp(velocity.x, 0.0, FRICTION_WEIGHT) if abs(velocity.x) > 1 else 0
+		
+	## Handle jump.
+	#if Input.is_action_just_pressed("jump"): # and is_on_floor():
+		#print("jumping")
+		#is_jumping = true
+		#velocity.y -= JUMP_VELOCITY
+	#else: 
+		#velocity += get_gravity() * delta
 	# Vertical control
 	if v_movement_direction != 0:
 		velocity.y = clamp(velocity.y + (v_movement_direction * ACCELERATION), -SPEED_LIMIT, SPEED_LIMIT)
 	else:
-		#print("v_movement_direction != 0 else")
 		velocity.y = lerp(velocity.y, 0.0, FRICTION_WEIGHT) if abs(velocity.y) > 1 else 0
 	
 	
+	is_attacking = _body_animations.current_animation == "attack_01"
+	is_jumping = _body_animations.current_animation == "jump"
 	
 	if h_movement_direction != 0 or v_movement_direction != 0:
 		move()
 	elif Input.is_action_just_pressed("attack_01"):
 		attack()
-	elif not is_moving and not is_attacking:
+	elif Input.is_action_just_pressed("jump"):
+		jump()
+	elif not is_moving and not is_attacking and not is_jumping:
 		_play_animation("idle")
-		
+	
+	move_and_slide()
 
 	
 func _play_animation(animation: String) -> void:
@@ -65,7 +71,6 @@ func _play_animation(animation: String) -> void:
 func move() -> void:
 	is_moving = true
 	_play_animation("run")
-	move_and_slide()
 	is_moving = false
 
 
@@ -73,6 +78,12 @@ func attack() -> void:
 	is_attacking = true
 	_play_animation("attack_01")
 	is_attacking = false
+
+
+func jump() -> void:
+	is_jumping = true
+	_play_animation("jump")
+	is_jumping = false
 
 
 func _on_jump_buffer_timer_timeout() -> void:
