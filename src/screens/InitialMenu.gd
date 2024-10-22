0b77c1c73561ffd9aedcecb8b709b902c67e0b11
B. Emmanuel Pericon <brianpericon.e92@gmail.com>
2024-10-22 15:55:10 -0300

    feat: some progress on first level

---
+++ src/screens/InitialMenu.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+@export var level_manager_scene: PackedScene
+
+func _ready() -> void:
+	pass
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_packed(level_manager_scene)
