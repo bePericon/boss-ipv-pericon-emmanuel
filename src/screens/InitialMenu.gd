e7c462ad762ec8e208b4fb20034964a4e581354c
B. Emmanuel Pericon <brianpericon.e92@gmail.com>
2024-10-27 14:49:21 -0300

    feat: some progress IPV-17: first stage sounds

--- src/screens/InitialMenu.gd
+++ src/screens/InitialMenu.gd
@@ -1,13 +1,26 @@
 extends Node
 
 @export var level_manager_scene: PackedScene
+@onready var select_fx: AudioStreamPlayer2D = $SelectFx
+@onready var confirmation_fx: AudioStreamPlayer2D = $ConfirmationFx
+
 
 func _ready() -> void:
-	pass
+	select_fx.stream = preload("res://assets/sounds/fx/select_001.ogg")
+	confirmation_fx.stream = preload("res://assets/sounds/fx/confirmation_003.ogg")
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_packed(level_manager_scene)
+	confirmation_fx.play()
 
 
 func _on_options_button_pressed() -> void:
 	pass # Replace with function body.
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_down") or event.is_action_pressed("ui_up"):
+		select_fx.play()
+
+
+func _on_confirmation_fx_finished() -> void:
+	get_tree().change_scene_to_packed(level_manager_scene)
